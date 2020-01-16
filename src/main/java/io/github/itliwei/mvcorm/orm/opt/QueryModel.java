eed17569cfffbe21a9fef12f60d51bf0d218647c
itliwei <itliwei@163.com>
2020-01-16 17:52:59 +0800

    修改为实体类

--- src/main/java/io/github/itliwei/mvcorm/orm/opt/QueryModel.java
+++ src/main/java/io/github/itliwei/mvcorm/orm/opt/QueryModel.java
@@ -9,13 +9,22 @@ import java.util.List;
  * @author : liwei
  * @date 2019/05/11 15:14
  */
-public abstract class QueryModel{
+public class QueryModel{
 
     private int pageNumber = 1;
 
     private int pageSize = 10;
 
     private List<OrderBy> orderBys;
+
+    public QueryModel(){
+
+    }
+
+    public QueryModel(int pageNumber,int pageSize){
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+    }
 
     public int getPageNumber() {
         return pageNumber;
